e2bde65f211bc9cee417ec9a7d20e6319803473e
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-14 16:36:50 +0330

    create setup.py

---
+++ tahaFirstapp/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tahaWebapp1',
+    version='',
+    packages=['blog', 'blog.migrations', 'blog.templatetags', 'tahaFirstapp'],
+    package_dir={'': 'tahaFirstapp'},
+    url='',
+    license='',
+    author='PcPlus',
+    author_email='',
+    description=''
+)

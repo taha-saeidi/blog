145983ea141be78602d73338d053cd9d9842e743
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-19 23:21:14 +0330

    settings

--- tahaFirstapp/tahaFirstapp/settings.py
+++ tahaFirstapp/tahaFirstapp/settings.py
@@ -70,17 +70,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tahaFirstapp.wsgi.application'
 
-
+#
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': 'taha1234',
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

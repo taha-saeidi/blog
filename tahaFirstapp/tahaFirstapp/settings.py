63e19804ae0fe389e9e37c1ed601739825b1cc9b
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-26 15:54:48 +0330

    postgres contrib add

--- tahaFirstapp/tahaFirstapp/settings.py
+++ tahaFirstapp/tahaFirstapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'django_jalali'
+    'django_jalali',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [

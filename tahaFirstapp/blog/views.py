122722c4bc246a9c1883e9e77fe9a7cc67f2627e
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-26 18:40:46 +0330

    upgrade post_search view

--- tahaFirstapp/blog/views.py
+++ tahaFirstapp/blog/views.py
@@ -115,8 +115,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_query = SearchQuery(query)
             # result = Post.Published_Manager.filter(Q(title__search=query) | Q(description__search=query))
-            result = Post.Published_Manager.annotate(search = SearchVector("title","description","slug")).filter(search = query)
+            result = Post.Published_Manager.annotate(search = SearchVector("title","description","slug")).filter(search = search_query)
     context = {'resault':result,'query':query}
     return render(request, 'blog/search.html',context)
 

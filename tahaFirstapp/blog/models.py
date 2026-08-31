ff7f171ce70248bd8be9de6346ca548043e53b06
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-31 08:16:49 +0330

    create image table

--- tahaFirstapp/blog/models.py
+++ tahaFirstapp/blog/models.py
@@ -1,4 +1,4 @@
-#moduls
+# moduls
 from django.utils import timezone
 
 from datetime import timezone
@@ -8,48 +8,57 @@ from django.utils import timezone, module_loading
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 from django.urls import reverse
-#manager
+
+
+# manager
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 # Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
-        PUBLISHED = "PB" , "Published"
-        DRAFT = "DF" , "Draft"
-        Rejected = "R" , "Rejected"
-    #relations
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post" , verbose_name = "نویسنده")
-    #data fields
-    title = models.CharField(max_length=250, verbose_name = "عنوان")
-    description = models.TextField(verbose_name = "توضیحات")
+        PUBLISHED = "PB", "Published"
+        DRAFT = "DF", "Draft"
+        Rejected = "R", "Rejected"
+
+    # relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post", verbose_name="نویسنده")
+    # data fields
+    title = models.CharField(max_length=250, verbose_name="عنوان")
+    description = models.TextField(verbose_name="توضیحات")
     slug = models.SlugField(max_length=250)
-    #date
+    # date
     published = jmodels.jDateTimeField(default=timezone.now)
-    created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "زمان تولید")
-    updated = jmodels.jDateTimeField(auto_now=True, verbose_name = "زمان آپدیت")
-    #choices fields
+    created = jmodels.jDateTimeField(auto_now_add=True, verbose_name="زمان تولید")
+    updated = jmodels.jDateTimeField(auto_now=True, verbose_name="زمان آپدیت")
+    # choices fields
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT
                               , verbose_name=" وضعیت پست"
                               )
-    reading_time = models.PositiveIntegerField(default=0 , verbose_name="زمان مطالعه")
+    reading_time = models.PositiveIntegerField(default=0, verbose_name="زمان مطالعه")
 
     # objects = models.Manager()
     objects = jmodels.jManager()
     Published_Manager = PublishedManager()
+
     def get_absolute_url(self):
         return reverse("blog:post_detail", args={self.id})
+
     class Meta:
         ordering = ('-published',)
         indexes = [
-        models.Index(fields=['-published']),
+            models.Index(fields=['-published']),
         ]
         verbose_name = "پست ها"
         verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.title
+
 
 class Ticket(models.Model):
     message = models.TextField()
@@ -61,60 +70,46 @@ class Ticket(models.Model):
     class Meta:
         verbose_name = "تیکت ها"
         verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.subject
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment" , verbose_name="پست")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment", verbose_name="پست")
     body = models.TextField(verbose_name="پیام")
     name = models.CharField(max_length=250, verbose_name="اسم")
     created = jmodels.jDateTimeField(auto_now_add=True, verbose_name="زمان تولید")
     updated = jmodels.jDateTimeField(auto_now=True, verbose_name="زمان آپدیت")
     # published = models.DateTimeField(default=timezone.now())
-    active = models.BooleanField(default=False,verbose_name="وضعیت")
+    active = models.BooleanField(default=False, verbose_name="وضعیت")
 
     class Meta:
         ordering = ('created',)
         indexes = [
-        models.Index(fields=['created']),
+            models.Index(fields=['created']),
         ]
         verbose_name = " کامنت ها "
         verbose_name_plural = verbose_name
+
     def __str__(self):
         return f"{self.name}:{self.post}"
 
 
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images", verbose_name="پست")
+    image_file = models.ImageField(upload_to="post_images/", verbose_name="تصویر")
+    title = models.CharField(max_length=250, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = jmodels.jDateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('created',)
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+        verbose_name = " تصویر ها "
+        verbose_name_plural = verbose_name
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def __str__(self):
+        return self.title if self.title else "None"
